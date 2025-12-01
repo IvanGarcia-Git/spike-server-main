bc9a753d5cef344d137edab5fb86bc9012c1ffa7
Iván García <garciayalaivan@gmail.com>
2025-12-01 10:10:29 +0100

    MySQL

--- app-data-source.ts
+++ app-data-source.ts
@@ -5,8 +5,12 @@ dotenv.config();
 const isProduction = process.env.NODE_ENV === "production";
 
 export const dataSource = new DataSource({
-  type: "sqlite",
-  database: process.env.DB_PATH || "spikes.db",
+  type: "mysql",
+  host: process.env.DB_HOST || "localhost",
+  port: parseInt(process.env.DB_PORT || "3306"),
+  username: process.env.DB_USERNAME || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_DATABASE || "spikes",
   entities: isProduction
     ? ["dist/src/models/*.entity.js"]
     : [__dirname + "/src/models/*.entity.ts"],
@@ -14,4 +18,5 @@ export const dataSource = new DataSource({
   synchronize: true,
   migrations: ["dist/src/migrations/**/*{.ts,.js}"],
   migrationsTableName: "migration",
+  charset: "utf8mb4",
 });
